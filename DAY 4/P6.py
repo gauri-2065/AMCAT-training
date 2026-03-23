1613de8209c00be99d98bf605e9d0296309f9103
gauri-2065 <24thakreg@rbunagpur.in>
2026-03-24 02:23:21 +0530

    Update and rename P6 to P6.py

---
+++ DAY 4/P6.py
@@ -0,0 +1,10 @@
+str1 = "listen"
+str2 = "silent"
+
+str1 = set(str1)
+str2 = set(str2)
+
+if str1 == str2:
+    print("Both strings are made of same characters.")
+else:
+    print("Both strings are not made of same characters.")
